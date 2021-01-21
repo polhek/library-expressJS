31162f384a071d954ce034745210369763cf3edd
polhek <ziga.groselj@gmail.com>
2021-01-21 19:13:16 +0100

    First commit

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 100 },
+});
+
+GenreSchema.virtual('url').get(() => {
+  return '/catalog/genre' + _id;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
