9b1056837391f9f1c141a65e90f37c4636213686
polhek <ziga.groselj@gmail.com>
2021-01-23 16:55:03 +0100

    Routing

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
-const pw = fucker22;
 
 // Mongoose connection
 var mongoose = require('mongoose');
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
